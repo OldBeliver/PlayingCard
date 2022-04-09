d91f2b71d42116a0a92c38df1021ca868b2d800a
OldBeliver <380506338004@yandex.ua>
2022-04-09 20:02:04 +0300

    Добавьте файлы проекта.

---
+++ PlayingCard/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayingCard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dealer dealer = new Dealer();
+            int number = 100;
+
+            Console.WriteLine($"дилер распечатал новую колоду карт");
+            dealer.CreateNewDeck();
+            dealer.ShowDeck();
+
+            
+            dealer.Shuffle(number);
+            Console.WriteLine($"\n... и перетасовал ее {number} раз");
+            dealer.ShowDeck();
+            
+            Console.WriteLine();
+
+            Console.WriteLine($"Сколько карт Вы желаете взять? ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < userInput; i++)
+            {
+                dealer.HandOver();
+            }
+            
+            Console.WriteLine($"Ваши карты");
+            dealer.ShowArm();
+        }
+    }
+
+    class Dealer
+    {        
+        private Deck _deck;
+        private Player _player;
+        private static Random _random;
+
+        static Dealer()
+        {
+            _random = new Random();
+        }
+
+        public Dealer()
+        {
+            _player = new Player();
+        }
+
+        public Deck CreateNewDeck()
+        {
+            _deck = new Deck();
+            string[] name = new string[] { "6", "7", "8", "9", "10", "В", "К", "Д", "Т" };
+            string[] suit = new string[] { "♠", "♣", "♦", "♥" };
+
+            for (int j = 0; j < name.Length; j++)
+            {
+                for (int i = 0; i < suit.Length; i++)
+                {
+                    string card = (name[j] + suit[i]);
+                    _deck.AddNewCard(new Card(card));
+                }
+            }
+
+            return _deck;
+        }
+
+        public void ShowDeck()
+        {            
+            _deck.ShowInfo();
+        }
+
+        public void ShowArm()
+        {
+            _player.ShowArm();
+        }
+
+        public void Shuffle(int quantity)
+        {
+            List<Card> deck = _deck.GetDeck();
+
+            for (int i = 0; i < quantity; i++)
+            {
+                int depth = _random.Next(deck.Count);
+                int range = _random.Next(1, deck.Count - depth);
+
+                Card[] tempSlice = new Card[range];
+
+                deck.CopyTo(depth, tempSlice, 0, range);
+                deck.RemoveRange(depth, range);
+                deck.InsertRange(0, tempSlice);
+            }
+        }
+
+        public void HandOver()
+        {
+            Card card =_deck.GetCard();
+            _deck.RemoveCard(card);
+            _player.TakeCard(card);
+        }
+    }
+
+    class Player
+    {
+        private Deck _arm;
+
+        public Player()
+        {
+             _arm = new Deck();
+        }
+
+        public void TakeCard(Card card)
+        {
+            _arm.AddNewCard(card);
+        }
+
+        public void ShowArm()
+        {
+            _arm.ShowInfo();
+        }
+    }
+
+    class Deck
+    {
+        private List<Card> _deck;
+
+        public Deck()
+        {
+            _deck = new List<Card>();
+        }
+
+        public void ShowInfo()
+        {            
+            for (int i = 0; i < _deck.Count; i++)
+            {
+                _deck[i].ToDysplay();
+            }
+        }
+
+        public void AddNewCard(Card card)
+        {
+            _deck.Add(card);
+        }
+
+        public void RemoveCard(Card card)
+        {
+            _deck.Remove(card);
+        }
+
+        public List<Card> GetDeck()
+        {
+            return _deck;
+        }
+
+        public Card GetCard()
+        {
+            int lastIndex = 0;
+
+            if (_deck.Count > 0)
+            {
+                lastIndex = _deck.Count - 1;
+            }
+            
+            return _deck[lastIndex];
+        }
+    }
+
+    class Card
+    {
+        public string Name { get; private set; }
+
+        public Card(string name)
+        {
+            Name = name;
+        }
+
+        public void ToDysplay()
+        {
+            Console.ForegroundColor = Colored(Name);
+            Console.Write($"{Name} ");
+        }        
+
+        private ConsoleColor Colored(string name)
+        {
+            return (name.Contains("♥") || name.Contains("♦")) ? ConsoleColor.Red : ConsoleColor.White;
+        }
+    }
+}
